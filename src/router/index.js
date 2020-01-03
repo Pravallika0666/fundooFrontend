82ad08fb0916d954e04e4d83f2a639235404fb36
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-03 15:20:37 +0530

    [Pravallika]add.created an fundoofrontend and pushed into remote

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import login from "../components/login";
+import register from "../components/register";
+import VueMaterial from 'vue-material'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial)
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: login
+  },
+  {
+    path:"/register",
+    name:"register",
+    component:register
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
