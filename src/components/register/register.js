772555784f54573341dc2bc993a3f0891c7db275
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-03 15:23:11 +0530

    [Pravallika]add.created an fundoo frontend register  and pushed into remote

---
+++ src/components/register/register.js
@@ -0,0 +1,29 @@
+
+export default {
+  name: 'register',
+  components: {},
+  props: [],
+  data () {
+    return {
+      loading: false,
+      register: {
+        firstName:"",
+        lastName:"",
+        email: "",
+        password: "",
+        confirmPassword:""
+      }
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
