3ee4129a743cd86aeb00f80594f32ad193962e4a
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-03 15:23:45 +0530

    [Pravallika]add.created an fundoo frontend forgotpassword  and pushed into remote

---
+++ src/components/forgotpassword/forgotpassword.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'forgotpassword',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
