9d18646f53cc8ed6dd7be482d0fdb6a6e5842413
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-08 18:18:56 +0530

    [Pravallika]refactor.modified changes for the forgot password component and pushe into remote

--- src/components/forgotpassword/forgotpassword.js
+++ src/components/forgotpassword/forgotpassword.js
@@ -1,23 +1,74 @@
-
+import { validationMixin } from 'vuelidate';
+import {
+  required
+} from 'vuelidate/lib/validators'
+import { getAll } from '../../services/services';
+import router from '../../router';
 export default {
   name: 'forgotpassword',
+  mixins: [validationMixin],
   components: {},
   props: [],
-  data () {
+  data() {
     return {
       loading: false,
-      login: {
+      forgotpassword: {
         email: ""
+      },
+      userSaved: false,
+      sending: false,
+      lastUser: null
+    }
+  },
+  validations: {
+    forgotpassword: {
+      email: {
+        required
       }
     }
   },
   computed: {
 
   },
-  mounted () {
-
+  mounted() {
   },
   methods: {
+    getValidationClass(fieldName) {
+      const field = this.$v.forgotpassword[fieldName]
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    clearForm() {
+      this.$v.$reset()
+      this.forgotpassword.email = null
+    },
+    saveUser() {
+      this.sending = true
+      console.log("forgotpassword",this.forgotpassword);
+      var obj={
+        email:this.forgotpassword.email
+      }
+      getAll('http://localhost:4000/forgotpassword',obj).then(res=>{
+        console.log("res",res)
+        router.push('/')
+      })
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.lastUser = `${this.forgotpassword.email}`
+        this.userSaved = true
+        this.sending = false
+        this.clearForm()
+      }, 150000)
+    },
+    validateUser() {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.saveUser()
+      }
+    }
 
   }
 }
