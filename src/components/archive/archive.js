5f8533ff90d340d029a8246e7fd8496e29da283f
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-08 18:19:52 +0530

    [Pravallika]add.added archive component and pushed into remote

---
+++ src/components/archive/archive.js
@@ -0,0 +1,31 @@
+import displayNote from "../displayNote";
+import { getAllnote } from "../../services/services"
+export default {
+  name: 'archive',
+  components: { displayNote },
+  props: [],
+  data() {
+    return {
+      archive: []
+    }
+  },
+  computed: {
+
+  },
+  mounted() {
+    getAllnote('http://localhost:4000/note/getAllnote').then(res => {
+      console.log("resssssss", res);
+      for (let index = 0; index < res.data.length; index++) {
+        if (res.data[index].isArchived == true) {
+          console.log("kdsdksdskdks;k", res.data[index]);
+          this.archive.push(res.data[index])
+        }
+      }
+    })
+  },
+  methods: {
+
+  }
+}
+
+
