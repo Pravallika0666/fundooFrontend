4f21b1e800605cf3b2ceccd49b6e2881c413575d
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-08 18:22:01 +0530

    [Pravallika]add.added take a note  component and pushed into remote

---
+++ src/components/takeAnote/index.vue
@@ -0,0 +1,4 @@
+<template src="./takeAnote.html"></template>
+<script src="./takeAnote.js"></script>
+<style src="./takeAnote.scss" scoped lang="scss"></style>
+
---
+++ src/components/takeAnote/takeAnote.js
@@ -0,0 +1,59 @@
+import icon from "../icon";
+import { addNote, messageService } from "../../services/services"
+import { validationMixin } from 'vuelidate'
+import {
+
+} from 'vuelidate/lib/validators';
+export default {
+  name: 'take-anote',
+  mixins: [validationMixin],
+  components: { icon },
+  props: [],
+  data() {
+    return {
+      loading: false,
+      flag: true,
+      takeNote: {
+        title: "",
+        description: "",
+      }, userSaved: false,
+      sending: false,
+      lastUser: null,
+      reminder:String,
+      pinned:false,
+      archive:String,
+      bgcolor:String,
+      delete:false
+    }
+  },
+  computed: {
+
+  },
+  mounted() {
+  },
+  methods: {
+    openCard() {
+      this.flag = !this.flag
+    },   
+  
+    addNote(){
+      this.flag = !this.flag
+      var note = {
+        title: this.takeNote.title,
+        description: this.takeNote.description,
+        Reminder: this.isreminder,
+        isPinned: this.isPinned,
+        isArchived: this.isArchive,
+        color: this.bgcolor,
+        isDeleted: this.delete
+      }
+      addNote('http://localhost:4000/note/addNote', note).then(res => {
+        console.log("addnote", res);
+        messageService.sendMessage("hiiiiiiiiiiii")
+      })
+
+    }
+  }
+}
+
+
