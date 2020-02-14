8f2839e0e6191474432c535162417a6b6896ff76
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-14 12:02:45 +0530

    [Pravallika]refactor.modified changes to the login component fundoo

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,7 +3,7 @@ import {
   required,
 } from 'vuelidate/lib/validators';
 import router from '../../router';
-import { getAll } from '../../services/services';
+import services from '../../services/services';
 export default {
   name: 'login',
   mixins: [validationMixin],
@@ -57,12 +57,13 @@ export default {
         email: this.user.email,
         password: this.user.password
       }
-      getAll('http://localhost:4000/login', obj).then(res => {
+      services.getAll('http://localhost:4000/login', obj).then(res => {
         console.log("res", res);
         localStorage.setItem('firstname', res.data[1].firstName)
         localStorage.setItem('lastname', res.data[1].lastName)
         localStorage.setItem('email', res.data[1].email)
         localStorage.setItem('_id', res.data[1]._id)
+        localStorage.setItem('token',res.data[0].token)
         router.push('/dashboard')
       })
       // Instead of this timeout, here you can call your API
