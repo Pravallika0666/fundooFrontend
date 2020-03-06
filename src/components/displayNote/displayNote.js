53ddd1e0fb009ae428566e21635e8396310cc535
Pravallika <skpravallikachowdary2@gmail.com>
2020-03-06 18:53:25 +0530

    [Pravallika]refactor.modified changes in display note

--- src/components/displayNote/displayNote.js
+++ src/components/displayNote/displayNote.js
@@ -1,6 +1,7 @@
 import icon from "../icon";
 import services, { messageService } from "../../services/services";
 import { EventBus } from "../../eventBus";
+import {SearchBus} from "../../eventBus";
 import draggable from "vuedraggable";
 export default {
   name: 'display-note',
@@ -10,24 +11,54 @@ export default {
   },
   data() {
     return {
+      nameLabel:String,
+      items: "",
+      collaboratorProfile: null,
+      image: String,
       event: '',
       showDialog: false,
+      collaboratorEmail: [],
+      collaborators: [],
       dialogData: Object,
       updatetitle: "",
       updatedescription: "",
       updateCard: Object,
       color: String,
-      grid: ""
+      grid: "",
+      query: "",
+      labelsArray:""
     }
   },
   computed: {
+    filterList(){
+      return this.cards.filter(card=>{
+        return (
+          card.title.toLowerCase().includes(this.query.toLowerCase()) ||
+          card.description.toLowerCase().includes(this.query.toLowerCase())
+        )
+      })
+    }
+  },
+  mounted() {
+    this.image = localStorage.getItem('image')
+    services.getLabels('http://localhost:4000/note/getLabels').then(res=>
+    this.labelsArray=res.data,
+    // console.log("response labelss",this.res)
+    
+    )
   },
   created() {
     EventBus.$on('i-got-clicked', grid => {
       this.grid = grid;
     });
+    SearchBus.$on("clicked", query => {
+      this.query = query;
+      console.log("search response",this.query);
+      
+    })
   },
   methods: {
+    // filterByLabel,
     archive(w) {
       let ind = this.cards.indexOf(w)
       this.cards.splice(ind, 1)
@@ -38,6 +69,9 @@ export default {
     trash(w) {
       let ind = this.cards.indexOf(w)
       this.cards.splice(ind, 1)
+    },
+    collaboratorAdd(card) {
+      this.$emit("collaborator", card)
     },
     unTrash(w) {
       this.trash(w)
@@ -79,6 +113,29 @@ export default {
         this.$emit("Reminder", card)
         messageService.sendMessage("message!!")
       })
+    },
+    labelChipDelete(card){
+      var object={
+        _id:card._id,
+        userId:card._id
+      }
+      services.deleteLabels("http://localhost:4000/note/deleteLabels",object).then(res=>{
+        console.log("chip delete labels",res);
+        this.$emit("nameLabel",card)
+        messageService.sendMessage("message!!")
+
+      })
+    },
+    collaboratorId(card) {
+      var object = {
+        
+        userId:card._id
+      }
+      services.getCollaborator('http://localhost:4000/note/getCollaborator', object).then(res =>
+        this.collaboratorEmail = res.data,
+        EventBus.$on('clicked', this.collaboratorEmail),
+        console.log("collabbbbbbbbiiiiiii", this.collaboratorEmail)
+      )
     },
 
   }
